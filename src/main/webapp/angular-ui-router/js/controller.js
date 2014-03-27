57f4ade20c117228b62a54bb9af8e67f64b2cdee
Em Rose <emrose@notonthehighstreet.com>
2014-03-27 17:32:17 +0000

    ng-router

---
+++ src/main/webapp/angular-ui-router/js/controller.js
@@ -0,0 +1,21 @@
+controllers.controller('LoginController', function ($scope, $rootScope, AUTH_EVENTS, AuthService) {
+  $scope.credentials = {
+    username: '',
+    password: ''
+  };
+  $scope.login = function (credentials) {
+    AuthService.login(credentials).then(function () {
+      $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
+    }, function () {
+      $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
+    });
+  };
+})
+.constant('AUTH_EVENTS', {
+  loginSuccess: 'auth-login-success',
+  loginFailed: 'auth-login-failed',
+  logoutSuccess: 'auth-logout-success',
+  sessionTimeout: 'auth-session-timeout',
+  notAuthenticated: 'auth-not-authenticated',
+  notAuthorized: 'auth-not-authorized'
+});
