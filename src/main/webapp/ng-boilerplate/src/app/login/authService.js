696a5f9446fcfbd96fffa5487010c2845feb64f7
emrose1 <emjrose@gmail.com>
2014-04-01 12:01:36 +0100

    ng-boilerplate

---
+++ src/main/webapp/ng-boilerplate/src/app/login/authService.js
@@ -0,0 +1,44 @@
+angular.module('authService', [] )
+.factory('authService', function ($http, Session) {
+	return {
+		login: function (credentials) {
+
+			gapi.client.booking.calendar.authUserSession(credentials).execute(function(resp) {
+				console.log('auth');
+				console.log(resp);
+
+			});
+
+			/*return $http
+			.post('/login', credentials)
+			.then(function (res) {
+			 	Session.create(res.id, res.userid, res.role);
+			});*/
+			console.log('bla');
+			Session.create('1234', '1234', 'admin');
+		},
+		isAuthenticated: function () {
+			return !!Session.userId;
+		},
+		isAuthorized: function (authorizedRoles) {
+			if (!angular.isArray(authorizedRoles)) {
+			authorizedRoles = [authorizedRoles];
+		}
+		return (this.isAuthenticated() &&
+			authorizedRoles.indexOf(Session.userRole) !== -1);
+		}
+	};
+})
+.service('Session', function () {
+	this.create = function (sessionId, userId, userRole) {
+		this.id = sessionId;
+		this.userId = userId;
+		this.userRole = userRole;
+	};
+	this.destroy = function () {
+		this.id = null;
+		this.userId = null;
+		this.userRole = null;
+	};
+	return this;
+});
